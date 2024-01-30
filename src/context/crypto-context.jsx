ea798582c3cafb98f03ed57fb92806f712023496
qulech <artttem826@gmail.com>
2024-01-30 20:36:56 +0500

    54

---
+++ src/context/crypto-context.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import { FetchAssests, fakeFetchCrypto } from '../components/layout/api';
+import { percentDifference } from "../utilus";
+const CryptoContext = createContext({
+    assets: [],
+    crypto:[],
+    loading:false,
+})
+
+export function CryptoContextProvider({children}) {
+    const [loading, setLoading] = useState(false)
+    const [crypto, setCrypto] = useState([])
+    const [assets, setAssets] = useState([])
+
+    useEffect(() => {
+        async function preload(){
+            setLoading(true)
+            const {result} = await fakeFetchCrypto()
+            const assets = await FetchAssests()
+
+            setAssets(assets.map(asset => {
+              const coin = result.find((c) => c.id == asset.id)
+              return{
+                grow: asset.price < coin.price,
+                growPercent: percentDifference(asset.price, coin.price),
+                totalAmount: asset.amount * coin.price,
+                totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+                ...asset,
+              }
+            }))
+            setCrypto(result)
+            setLoading(false)
+        }
+        preload()
+    },[])
+    return <CryptoContext.Provider value={{loading, crypto, assets}}>{children}</CryptoContext.Provider>
+}
+
+export default CryptoContext;
+
+
+export function useCrypto() {
+    return useContext(CryptoContext)
+  }
